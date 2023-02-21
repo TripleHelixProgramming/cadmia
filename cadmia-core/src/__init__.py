17e9369d397525eda7a54411ad4962c9ddc96b1c
Joshua Nichols <treehousejosh@gmail.com>
2023-02-21 15:43:03 -0500

    Add Aruco detection

--- cadmia-core/src/__init__.py
+++ cadmia-core/src/__init__.py
@@ -1,7 +1,8 @@
 import cv2 as cv
 
 # Runnable application file of cadmia
-while True:
+
+def main():
     # Get all available cameras
     cameras = []
     for camera_port in range(10):
@@ -9,3 +10,30 @@ while True:
         # If camera exists, add it to the list of stored cameras
         if (cap.isOpened()):
             cameras.append(cap)
+
+    while True:
+        # Capture camera frames
+        # TODO: first set camera config - e.g. exposure, brightness, etc
+        frames = []
+        for camera in cameras:
+            success, frame = camera.read()
+            if success:
+                frames.append(frame)
+        
+        # Detect Aruco markers
+        dictionary = cv.aruco.getPredefinedDictionary(cv.aruco.DICT_APRILTAG_16h5)
+        parameters = cv.aruco.DetectorParameters()
+        detector = cv.aruco.ArucoDetector(dictionary, parameters)
+        for frame in frames:
+            corners, ids, _ = detector.detectMarkers(frame)
+            cv.aruco.drawDetectedMarkers(frame, corners, ids)
+
+        # Display camera streams
+        img = cv.hconcat(frames)
+        cv.imshow('stream', img)
+
+        if cv.waitKey(1) & 0xFF == ord('q'):
+            break
+
+if __name__ == "__main__":
+    main()
