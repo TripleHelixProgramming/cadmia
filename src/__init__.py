008f2e8723bc7484cb7ef32157fec76ee98a4a07
Joshua Nichols <joshuanichols@joshuas-mbp.mynetworksettings.com>
2023-02-25 00:09:04 -0500

    Implement NT4 networking

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,8 @@
 import cv2 as cv
 from stream import Stream
 import imutils
+import time
+from network_tables_io import NetworkTablesIO
 
 # Runnable application file of cadmia
 
@@ -8,30 +10,39 @@ def main():
     # Initialize video stream
     stream = Stream(8080)
 
+    # Initialize NT4 client
+    client = NetworkTablesIO()
+
     # Get all available cameras
-    cameras = [cv.VideoCapture(0)]
-    # for camera_port in range(10)
-    #     cap = cv.VideoCapture("/dev/video" + str(camera_port))
-    #     # If camera exists, add it to the list of stored cameras
-    #     if (cap.isOpened()):
-    #         cameras.append(cap)
+    cameras = []
+    for camera_port in range(5):
+        cap = cv.VideoCapture(camera_port)
+        cameras.append(cap)
 
     while True:
         # Capture camera frames
         # TODO: first set camera config - e.g. exposure, brightness, etc
         frames = []
+        capture_times = []
         for camera in cameras:
-            success, frame = camera.read()
-            if success:
-                frames.append(frame)
+            if camera.isOpened():
+                success, frame = camera.read()
+                if success:
+                    frames.append(frame)
+                    capture_times.append(client.get_time())
         
         # Detect Aruco markers
         dictionary = cv.aruco.getPredefinedDictionary(cv.aruco.DICT_APRILTAG_16h5)
         parameters = cv.aruco.DetectorParameters()
         detector = cv.aruco.ArucoDetector(dictionary, parameters)
-        for frame in frames:
+        for index in range(len(frames)):
+            time = capture_times[index]
+            frame = frames[index]
             corners, ids, _ = detector.detectMarkers(frame)
             cv.aruco.drawDetectedMarkers(frame, corners, ids)
+            # Publish result to NetworkTables
+            if len(corners) > 0:
+                client.publish_result(index, time, corners, ids)
 
         # Display camera streams
         resized_frames = []
---
+++ src/network_tables_io.py
@@ -0,0 +1,26 @@
+import ntcore
+
+class NetworkTablesIO:
+    def __init__(self):
+        # Initialize NT4 client
+        self.inst = ntcore.NetworkTableInstance.getDefault()
+        self.table = self.inst.getTable("cadmia")
+        self.inst.startClient4("cadmia_client")
+        self.inst.setServer("192.168.1.153")
+
+        self.publishers = []
+        for index in range(5):
+            self.publishers.append(self.table.getDoubleArrayTopic("video" + str(index)).publish(
+                ntcore.PubSubOptions(periodic=0, sendAll=True, keepDuplicates=True)))
+
+    def get_time(self):
+        return ntcore._now()
+
+    def publish_result(self, index, time, corners, ids):
+        result = []
+        for tag_index in range(len(corners)):
+            result.append(ids[tag_index][0])
+            for corner in corners[tag_index][0]:
+                result.append(corner[0])
+                result.append(corner[1])
+        self.publishers[index].set(result, time)
